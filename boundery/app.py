211c297e6ff9ae7b2b5d6d663269bcecd779fb68
Nolan Leake <nolan@sigbus.net>
2019-02-22 13:54:08 -0800

    Make listen port a const.

--- boundery/app.py
+++ boundery/app.py
@@ -4,11 +4,14 @@ import webbrowser
 from bottle import route, run
 from threading import Thread
 
-#XXX Try random ports until one works.
+#XXX Try random ports until one works.  Bottle doesn't make this easy, since
+#    we don't know if a port can be bound until we're deep in run, and thus
+#    can't run our own code...
 
 #XXX Need some way to pull in other files with @route decorators.
 
 POLL_INTERVAL = 0.5
+PORT=1337
 
 @route('/server_ok')
 def root():
@@ -24,12 +27,12 @@ def poll_ready():
         except:
             pass
         time.sleep(POLL_INTERVAL)
-    webbrowser.open("http://localhost:8080/")
+    webbrowser.open("http://localhost:%s/" % PORT)
 
 poller = Thread(target=poll_ready, name="poll_ready", daemon=True)
 poller.start()
 
 if "--debug" in sys.argv:
-    run(host="localhost", port=8080, debug=True)
+    run(host="localhost", port=PORT, debug=True)
 else:
-    run(host='0.0.0.0', port=8080, debug=False, server='waitress')
+    run(host='0.0.0.0', port=PORT, debug=False, server='waitress')
